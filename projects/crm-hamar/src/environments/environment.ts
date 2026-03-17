8f645f74027fd020be5aef9832dce37107c82a1a
Claudia Heredia <cheredia.colegio@gmail.com>
2026-03-17 18:37:11 -0500

    fix: incluir environment.ts para build de Vercel

---
+++ projects/crm-hamar/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  supabaseUrl: 'https://sycyrvmepejmhqkvpisp.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN5Y3lydm1lcGVqbWhxa3ZwaXNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQzMzY2MjQsImV4cCI6MjA3OTkxMjYyNH0.4LarS74cqfgXWi4MgyAjunRgWsKj_0ja3RuBNrrAROM',
+  cloudtalkBaseUrl: 'https://sycyrvmepejmhqkvpisp.supabase.co/functions/v1'
+};
